c24c28376598c4b00df186b3fd8a9329ff1d7cdb
tappasarn <time723@gmail.com>
2019-08-08 15:23:59 +0700

    finish without testing

--- src/entropy.py
+++ src/entropy.py
@@ -2,6 +2,7 @@
 This module is use for entropy calculation
 """
 import numpy as np
+import math
 
 def calculate_entropy_simple(data, nCategories, labels):
     number_of_features = data.shape[1]
@@ -12,10 +13,24 @@ def calculate_entropy_simple(data, nCategories, labels):
 
     # Calculate entropy as sum of the entrpies for each feature in a cluster
     # and sum of the entropies of the clusters
-
     cluster_entropies = np.zeros((1, number_of_cluster))
 
     for i in range(number_of_cluster):
         j = unique_labels[i]
         idx = np.where(labels == j)[0]
+        data_cluster = data[idx, :]
 
+        for j in range(number_of_features):
+                temp = data_cluster[: , j]
+                n = len(temp)
+                unique_vals = np.unique(temp)
+
+                for k in range(len(unique_vals)):
+                    m = len(np.where(temp == unique_vals[k]))
+                    p = m/n
+                    cluster_entropies[i] = cluster_entropies[i] - p * math.log(p)
+						
+	for i in range(number_of_cluster):
+		total_entropy = total_entropy + cluster_entropies[i]
+	
+	return total_entropy
