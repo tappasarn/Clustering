95eae071474ad135ac2f6ec78ce1b2dd29e9d6ac
tappasarn <time723@gmail.com>
2019-08-08 17:17:56 +0700

    remove print in main

---
+++ src/main.py
@@ -0,0 +1,9 @@
+"""
+Project main file
+"""
+import k_mediods
+
+NUMBER_OF_CLUSTER = 2
+INPUT_FILE_PATH = '../data/random_1000_5.txt'
+
+# print(k_mediods.calculate(NUMBER_OF_CLUSTER, INPUT_FILE_PATH))
