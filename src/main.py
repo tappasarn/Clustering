1dd7e102302cb37fdad3f9a2f7a8c0d3eea73d45
tappasarn <time723@gmail.com>
2019-08-08 20:30:42 +0700

    update main

--- src/main.py
+++ src/main.py
@@ -2,8 +2,15 @@
 Project main file
 """
 import k_mediods
+import entropy
+import input_reader
 
-NUMBER_OF_CLUSTER = 2
+NUMBER_OF_CLUSTER = 6
 INPUT_FILE_PATH = '../data/random_1000_5.txt'
 
-# print(k_mediods.calculate(NUMBER_OF_CLUSTER, INPUT_FILE_PATH))
+data = input_reader.read_csv_input(INPUT_FILE_PATH)
+label = k_mediods.calculate(NUMBER_OF_CLUSTER, INPUT_FILE_PATH)
+
+total_entropy = entropy.calculate_entropy_simple(data, label)
+
+print('total entropy: ', total_entropy)
