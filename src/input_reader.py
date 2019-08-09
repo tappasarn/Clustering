abe3fd256b49272fa75c142441242909c0641753
tappasarn <time723@gmail.com>
2019-08-09 13:34:54 +0700

    use custom encoder

--- src/input_reader.py
+++ src/input_reader.py
@@ -3,19 +3,28 @@ This module is a wrapper for csv input reader
 It returns data which read from csv file as NumPy Array
 """
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
 
 def read_csv_input(file_name):
     # Read data from file
     df = pd.read_csv(file_name, sep=',',header=None)
 
-    # Our data is not number we should transform it to integer
-    # so we can test the output with other modules on the internet
+    # encode all input types to interger so that it is in a generic form
+    # for distance calculation
     return encode_string_input_to_integer(df.values)
 
-    # return df.values
-
 def encode_string_input_to_integer(input_arr):
-    enc = OneHotEncoder(handle_unknown='ignore')
-    transformed_data = enc.fit_transform(input_arr).toarray()
+    num_data, num_features = input_arr.shape
+    num_categories = []
+    transformed_data = np.zeros((num_data, num_features))
+    for i in range(num_categories):
+        one_column = input_arr[:,i]
+        uq_values = np.unique(one_column) 
+        num_categories[i] = len(uq_values)
+        temp = []
+        for j in range(uq_values):
+            idx = np.where(one_column==uq_values[j])[0]
+            temp = np.array(one_column)
+            temp[idx] = j
+        transformed_data[:,i] = temp
+    
     return transformed_data
