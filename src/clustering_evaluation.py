f7d2f6a1160d10983d0883b089925cb19200b85c
tappasarn <time723@gmail.com>
2019-08-16 01:57:24 +0700

    update SSE equation

---
+++ src/clustering_evaluation.py
@@ -0,0 +1,26 @@
+"""
+clustering evaluation measures library
+
+"""
+import numpy as np
+
+def SSE_distances(data, labels, pair_wise_distances):
+    
+    unique_labels = np.unique(labels)
+    number_of_cluster = len(unique_labels)
+    
+    SSE_clusters = np.zeros(number_of_cluster)
+    SSE_temp = 0
+    
+    for i in range(number_of_cluster):
+        j = unique_labels[i]
+        indices = np.where(labels == j)[0]
+        cluster_size = len(indices)
+        
+        clusters_pair_distances = pair_wise_distances[np.ix_(indices, indices)]
+        np.power(clusters_pair_distances, 2)
+        SSE_temp = SSE_temp + np.sum(clusters_pair_distances) / cluster_size
+        
+    return SSE_temp
+
+
